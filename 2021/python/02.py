ccf5dd4c5da908b341efb28d3c2857d826523154
Simon Sawicki <37424085+Grub4K@users.noreply.github.com>
2021-12-10 10:43:22 +0100

    Add 2021-02-python

---
+++ 2021/python/02.py
@@ -0,0 +1,32 @@
+def process_line(line):
+    command, _, count = line.partition(' ')
+    count = int(count)
+    return command, count
+
+
+with open('../input/02.txt') as file:
+    data = list(map(process_line, file))
+
+x = depth = 0
+for command, count in data:
+    if command=='forward':
+        x += count
+    elif command=='down':
+        depth += count
+    elif command=='up':
+        depth -= count
+
+print(x * depth)
+
+
+x = depth = aim = 0
+for command, count in data:
+    if command=='forward':
+        x += count
+        depth += aim * count
+    elif command=='down':
+        aim += count
+    elif command=='up':
+        aim -= count
+
+print(x * depth)
