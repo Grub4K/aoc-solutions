72cc1a6932b65f5cf6ef8bd7867ae71c8b693212
Simon Sawicki <37424085+Grub4K@users.noreply.github.com>
2021-12-13 18:42:44 +0100

    2021-07-batch

---
+++ 2021/python/07.py
@@ -0,0 +1,23 @@
+with open('../input/07.txt') as file:
+    line = file.readline()
+
+values = [*map(int, line.split(','))]
+value_range = range(min(values), max(values)+1)
+
+
+def distance(a):
+    return lambda b: abs(a-b)
+
+
+print(min(sum(map(distance(a), values))
+    for a in value_range))
+
+
+def fuel_distance(a):
+    def _fuel(b):
+        n = abs(a-b)
+        return n*(n+1)//2
+    return _fuel
+
+print(min(sum(map(fuel_distance(a), values))
+    for a in value_range))
