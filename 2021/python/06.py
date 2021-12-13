6b59e21473f06af9f95e527747b01714b55fda36
Simon Sawicki <37424085+Grub4K@users.noreply.github.com>
2021-12-13 16:44:56 +0100

    2021-06-python

---
+++ 2021/python/06.py
@@ -0,0 +1,28 @@
+from collections import Counter
+
+
+
+with open('../input/06.txt') as file:
+    line = file.readline()
+
+counter_dict = Counter(map(int, line.split(',')))
+fish_counter = [counter_dict.get(index, 0) for index in range(9)]
+
+
+from_ = 0
+to = 6
+
+for _ in range(80):
+    to = (to + 1) % 9
+    fish_counter[to] += fish_counter[from_]
+    from_ = (from_ + 1) % 9
+
+print(sum(fish_counter))
+
+
+for _ in range(256 - 80):
+    to = (to + 1) % 9
+    fish_counter[to] += fish_counter[from_]
+    from_ = (from_ + 1) % 9
+
+print(sum(fish_counter))
