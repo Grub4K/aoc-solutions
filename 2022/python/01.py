d2d0c91f6fb7780e4fa8fb40ce00c375fa0ed5a1
Simon Sawicki <contact@grub4k.xyz>
2022-12-02 04:07:16 +0100

    2022-01-python

---
+++ 2022/python/01.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+def process_data(lines):
+    result = [0]
+    for line in lines:
+        if not line:
+            result.append(0)
+            continue
+
+        result[-1] += int(line)
+
+    return result
+
+
+def run(data_input):
+    largest = heapq.nlargest(3, data_input)
+    yield largest[0]
+    yield sum(largest)
