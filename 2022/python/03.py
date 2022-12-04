a5d590c12f727fddbcf2feb7f51387f0cdf3bd93
Simon Sawicki <contact@grub4k.xyz>
2022-12-04 16:38:32 +0100

    2022-03-python

---
+++ 2022/python/03.py
@@ -0,0 +1,23 @@
+import string
+from functools import reduce
+
+LOOKUP = dict(zip(string.ascii_letters, range(1, 26 * 2 + 1)))
+
+
+def grouped(iterable, n):
+    return zip(*[iter(iterable)] * n)
+
+
+def run(data: list[str]):
+    accum = 0
+    for line in data:
+        half = len(line) // 2
+        common_element = set(line[:half]).intersection(line[half:]).pop()
+        accum += LOOKUP[common_element]
+    yield accum
+
+    GROUP_SIZE = 3
+    yield sum(
+        LOOKUP[reduce(set.intersection, group).pop()]
+        for group in grouped(map(set, data), GROUP_SIZE)
+    )
