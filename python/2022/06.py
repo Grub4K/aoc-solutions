9668cab7795ac2912bf6cfe5a1ad58b01bf2b0d0
Simon Sawicki <contact@grub4k.xyz>
2022-12-07 03:53:27 +0100

    python-2022-06

---
+++ python/2022/06.py
@@ -0,0 +1,17 @@
+from utils import nwise
+
+
+def process_data(data):
+    return data[0]
+
+
+def are_unique_items(sequence):
+    return len(sequence) == len(set(sequence))
+
+
+def run(data: list[tuple[str, str, str, str]]):
+    for size in 4, 14:
+        for index, items in enumerate(nwise(data, size)):
+            if are_unique_items(items):
+                yield index + size
+                break
