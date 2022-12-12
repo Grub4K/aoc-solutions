12140aac133ee71ec4b0b3a4bab5391703060f43
Simon Sawicki <contact@grub4k.xyz>
2022-12-12 02:56:44 +0100

    python-utils: Add direction and direction_range

--- python/utils.py
+++ python/utils.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+from enum import Enum
 from itertools import chain, pairwise, tee
 
 _SENTINEL = object()
@@ -19,10 +21,11 @@ def nwise(iterable, n):
 
 
 def first(iterable, default=_SENTINEL):
+    iterator = iter(iterable)
     if default is _SENTINEL:
-        return next(iter(iterable))
+        return next(iterator)
 
-    return next(iter(iterable), default)
+    return next(iterator, default)
 
 
 def grouped(iterable, n):
@@ -31,3 +34,37 @@ def grouped(iterable, n):
 
 def flatten(iterable):
     return chain.from_iterable(iterable)
+
+
+Offset = namedtuple("Offset", ["x", "y"])
+
+
+class Direction(Enum):
+    UP_LEFT = Offset(-1, -1)
+    UP = Offset(0, -1)
+    UP_RIGHT = Offset(1, -1)
+    LEFT = Offset(-1, 0)
+    RIGHT = Offset(1, 0)
+    DOWN_LEFT = Offset(-1, 1)
+    DOWN = Offset(0, 1)
+    DOWN_RIGHT = Offset(1, 1)
+
+
+CARDINAL_DIRECTIONS = (Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT)
+
+
+def make_direction_range(height, width, offset):
+    def dir_range(x, y):
+        x += x_offset
+        y += y_offset
+        while 0 <= x < width and 0 <= y < height:
+            yield x, y
+            x += x_offset
+            y += y_offset
+
+    x_offset, y_offset = offset.value if isinstance(offset, Direction) else offset
+    return dir_range
+
+
+def make_direction_ranges(height, width, *directions):
+    return tuple(make_direction_range(height, width, offset) for offset in directions)
--- python/utils.pyi
+++ python/utils.pyi
@@ -1,4 +1,5 @@
-from typing import Iterable, Literal, TypeVar, overload
+from enum import Enum
+from typing import Callable, Iterable, Literal, NamedTuple, TypeVar, overload
 
 T = TypeVar("T")
 U = TypeVar("U")
@@ -36,3 +37,26 @@ def grouped(
 @overload
 def grouped(iterable: Iterable[T], n: int) -> Iterable[tuple[T]]: ...
 def flatten(iterable: Iterable[Iterable[T]]) -> Iterable[T]: ...
+
+class Offset(NamedTuple):
+    x: int
+    y: int
+
+class Direction(Enum):
+    UP = Offset(0, -1)
+    UP_LEFT = Offset(-1, -1)
+    UP_RIGHT = Offset(1, -1)
+    DOWN = Offset(0, 1)
+    DOWN_LEFT = Offset(-1, 1)
+    DOWN_RIGHT = Offset(1, 1)
+    LEFT = Offset(-1, 0)
+    RIGHT = Offset(0, 1)
+
+CARDINAL_DIRECTIONS = (Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT)
+
+def make_direction_range(
+    height: int, width: int, *direction: Direction | Offset | tuple[int, int]
+) -> Callable[[int, int], Iterable[tuple[int, int]]]: ...
+def make_direction_ranges(
+    height: int, width: int, *directions: Direction | Offset | tuple[int, int]
+) -> tuple[Callable[[int, int], Iterable[tuple[int, int]]], ...]: ...
