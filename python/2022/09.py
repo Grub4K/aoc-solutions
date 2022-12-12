455f7c93588a0de8a372bdbfe183051095651198
Simon Sawicki <contact@grub4k.xyz>
2022-12-12 05:25:26 +0100

    python-2022-09

---
+++ python/2022/09.py
@@ -0,0 +1,48 @@
+import math
+
+from utils import Direction, Vector, flatten
+
+LOOKUP = {
+    "R": Direction.RIGHT,
+    "U": Direction.UP,
+    "L": Direction.LEFT,
+    "D": Direction.DOWN,
+}
+
+
+def process_line(line):
+    direction, _, amount = line.partition(" ")
+    return [LOOKUP[direction].value] * int(amount)
+
+
+def process_data(data):
+    return list(flatten(data))
+
+
+def move_knots(knots, movement):
+    previous = None
+    for knot in knots:
+        if previous is None:
+            knot += movement
+        else:
+            delta = previous - knot
+            if abs(delta.x) > 1 or abs(delta.y) > 1:
+                knot += (
+                    Vector(tuple(int(math.copysign(1, value)) for value in delta))
+                    if delta.x and delta.y
+                    else delta // 2
+                )
+
+        yield knot
+        previous = knot
+
+
+def run(movements: list[Vector]):
+    for length in 2, 10:
+        positions = set()
+        knots = [Vector()] * length
+        for movement in movements:
+            knots = list(move_knots(knots, movement))
+            positions.add(knots[-1])
+
+        yield len(positions)
