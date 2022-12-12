9ee46463a59bae4f5ee3808ce0f0d9df1cab26fe
Simon Sawicki <contact@grub4k.xyz>
2022-12-12 05:05:42 +0100

    python-utils: Implement Vector class

--- python/2022/08.py
+++ python/2022/08.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-from utils import CARDINAL_DIRECTIONS, make_direction_ranges
+from utils import CARDINAL_DIRECTIONS, make_direction_ranges, range2d
 
 
 def process_line(line):
@@ -19,28 +19,28 @@ def run(data: list[list[int]]):
     def calculate_slice(range_, tree):
         visible = True
         scenic_score = 0
-        for x_new, y_new in range_:
+        for position in range_:
             scenic_score += 1
 
-            if data[y_new][x_new] >= tree:
+            if data[position.y][position.x] >= tree:
                 visible = False
                 break
 
         return Result(scenic_score, visible)
 
-    def process_tree(x, y):
-        tree = data[y][x]
+    def process_tree(position):
+        tree = data[position.y][position.x]
 
         visible = False
         scenic_score = 1
         for direction_range in up, down, left, right:
-            result = calculate_slice(direction_range(x, y), tree)
+            result = calculate_slice(direction_range(position), tree)
 
             visible |= result.visible
             scenic_score *= result.scenic_score
 
         return Result(scenic_score, visible)
 
-    results = [process_tree(x, y) for y in range(height) for x in range(width)]
+    results = list(map(process_tree, range2d(width, height)))
     yield sum(result.visible for result in results)
     yield max(result.scenic_score for result in results)
--- python/utils.py
+++ python/utils.py
@@ -1,5 +1,6 @@
-from collections import namedtuple
+import math
 from enum import Enum
+from functools import cached_property
 from itertools import chain, pairwise, tee
 
 _SENTINEL = object()
@@ -36,33 +37,207 @@ def flatten(iterable):
     return chain.from_iterable(iterable)
 
 
-Offset = namedtuple("Offset", ["x", "y"])
+class Vector:
+    __slots__ = ("_values",)
+
+    def __init__(self, x=0, y=0, /):
+        if isinstance(x, tuple):
+            if len(x) != 2:
+                raise ValueError("Vector init length is expected to be 2")
+
+            x, y = x
+
+        self._values = (x, y)
+
+    @property
+    def x(self, /):
+        return self._values[0]
+
+    @property
+    def y(self, /):
+        return self._values[1]
+
+    @cached_property
+    def length(self, /):
+        return math.hypot(*self)
+
+    def normalize(self, /):
+        return self.__class__(self.x / self.length, self.y / self.length)
+
+    def __mul__(self, other, /):
+        if not isinstance(other, int | float):
+            return NotImplemented
+
+        return self.__class__(self.x * other, self.y * other)
+
+    def __rmul__(self, other, /):
+        return self.__mul__(other)
+
+    def __truediv__(self, other, /):
+        if not isinstance(other, int | float):
+            return NotImplemented
+
+        return self.__class__(self.x / other, self.y / other)
+
+    def __floordiv__(self, other, /):
+        if not isinstance(other, int | float):
+            return NotImplemented
+
+        return self.__class__(self.x // other, self.y // other)
+
+    def __add__(self, other, /):
+        if isinstance(other, Vector):
+            return self.__class__(self.x + other.x, self.y + other.y)
+
+        elif isinstance(other, int | float):
+            return self.__class__(self.x + other, self.y + other)
+
+        return NotImplemented
+
+    def __radd__(self, other, /):
+        return self.__add__(other)
+
+    def __sub__(self, other, /):
+        if isinstance(other, Vector):
+            return self.__class__(self.x - other.x, self.y - other.y)
+
+        elif isinstance(other, int | float):
+            return self.__class__(self.x - other, self.y - other)
+
+        return NotImplemented
+
+    def __rsub__(self, other, /):
+        return self.__sub__(other)
+
+    def __lt__(self, other, /):
+        if isinstance(other, int | float):
+            return self.x < other > self.y
+
+        elif isinstance(other, Vector):
+            return self.x < other.x and self.y < other.y
+
+        elif isinstance(other, tuple):
+            if len(other) != 2:
+                return NotImplemented
+            return self.x < other[0] and self.y < other[1]
+
+        return NotImplemented
+
+    def __le__(self, other, /):
+        if isinstance(other, int | float):
+            return self.x <= other >= self.y
+
+        elif isinstance(other, Vector):
+            return self.x <= other.x and self.y <= other.y
+
+        elif isinstance(other, tuple):
+            if len(other) != 2:
+                return NotImplemented
+            return self.x <= other[0] and self.y <= other[1]
+
+        return NotImplemented
+
+    def __eq__(self, other, /):
+        if isinstance(other, int | float):
+            return self.x == other == self.y
+
+        elif isinstance(other, Vector):
+            return self.x == other.x and self.y == other.y
+
+        elif isinstance(other, tuple):
+            if len(other) != 2:
+                return NotImplemented
+            return self.x == other[0] and self.y == other[1]
+
+        return NotImplemented
+
+    def __ne__(self, other, /):
+        if isinstance(other, int | float):
+            return self.x != other != self.y
+
+        elif isinstance(other, Vector):
+            return self.x != other.x and self.y != other.y
+
+        elif isinstance(other, tuple):
+            if len(other) != 2:
+                return NotImplemented
+            return self.x != other[0] and self.y != other[1]
+
+        return NotImplemented
+
+    def __ge__(self, other, /):
+        if isinstance(other, int | float):
+            return self.x >= other <= self.y
+
+        elif isinstance(other, Vector):
+            return self.x >= other.x and self.y >= other.y
+
+        elif isinstance(other, tuple):
+            if len(other) != 2:
+                return NotImplemented
+            return self.x >= other[0] and self.y >= other[1]
+
+        return NotImplemented
+
+    def __gt__(self, other, /):
+        if isinstance(other, int | float):
+            return self.x > other < self.y
+
+        elif isinstance(other, Vector):
+            return self.x > other.x and self.y > other.y
+
+        elif isinstance(other, tuple):
+            if len(other) != 2:
+                return NotImplemented
+            return self.x > other[0] and self.y > other[1]
+
+        return NotImplemented
+
+    def __str__(self, /):
+        return f"<{self.x}|{self.y}>"
+
+    def __iter__(self, /):
+        return iter(self._values)
+
+    def __repr__(self, /):
+        return f"Vector(x={self.x}, y={self.y})"
+
+    def __hash__(self, /):
+        return self._values.__hash__()
 
 
 class Direction(Enum):
-    UP_LEFT = Offset(-1, -1)
-    UP = Offset(0, -1)
-    UP_RIGHT = Offset(1, -1)
-    LEFT = Offset(-1, 0)
-    RIGHT = Offset(1, 0)
-    DOWN_LEFT = Offset(-1, 1)
-    DOWN = Offset(0, 1)
-    DOWN_RIGHT = Offset(1, 1)
+    UP_LEFT = Vector(-1, -1)
+    UP = Vector(0, -1)
+    UP_RIGHT = Vector(1, -1)
+    LEFT = Vector(-1, 0)
+    RIGHT = Vector(1, 0)
+    DOWN_LEFT = Vector(-1, 1)
+    DOWN = Vector(0, 1)
+    DOWN_RIGHT = Vector(1, 1)
 
 
 CARDINAL_DIRECTIONS = (Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT)
 
 
-def make_direction_range(height, width, offset):
-    def dir_range(x, y):
-        x += x_offset
-        y += y_offset
-        while 0 <= x < width and 0 <= y < height:
-            yield x, y
-            x += x_offset
-            y += y_offset
+def range2d(width, height):
+    for y in range(height):
+        for x in range(width):
+            yield Vector(x, y)
 
-    x_offset, y_offset = offset.value if isinstance(offset, Direction) else offset
+
+def make_direction_range(height, width, offset):
+    def dir_range(position):
+        position += offset
+        while (0, 0) <= position < (width, height):
+            yield position
+            position += offset
+
+    if isinstance(offset, Direction):
+        offset = offset.value
+    elif isinstance(offset, tuple):
+        offset = Vector(*offset)
+
     return dir_range
 
 
